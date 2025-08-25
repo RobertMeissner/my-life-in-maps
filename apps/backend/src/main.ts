bbeccfe7edaa9393c872c20937bdff5f3c8ffc9d
Robert Meissner <50482279+RobertMeissner@users.noreply.github.com>
2025-08-25 15:06:13 +0200

    Feat: added auto swagger UI creation to nestjs backend

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('My Life in Maps API')
+    .setDescription('API description')
+    .setVersion('1.0.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   app.enableCors({ origin: 'http://localhost:5173', credentials: true });
   await app.listen(process.env.PORT ?? 3000);
 }
